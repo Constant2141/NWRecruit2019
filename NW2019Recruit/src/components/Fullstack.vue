836ba818fdabafdfa79239a02fab33f61c87abc0
KwokChikuan <chikuan@MacdeMacBook-Pro-6.local>
2019-02-15 15:50:05 +0800

    大坝

--- NW2019Recruit/src/components/Fullstack.vue
+++ NW2019Recruit/src/components/Fullstack.vue
@@ -15,6 +15,8 @@
       </div>
       <div class="cans" ref="can">
         <canvas id="can"></canvas>
+        <img src="../assets/ball.png" id="ball" v-show="false">
+        <canvas id="canv"></canvas>
       </div>
     </div>
 </template>
@@ -23,11 +25,13 @@
 export default {
   data(){
     return{
-
+      aw1:0,aw2:0,aw3:0,aw4:0,aw5:0,aw6:0,aw7:0,aw8:0,aw9:0,aw10:0,aw11:0,aw12:0,aw13:0,aw14:0,
+      ah1:0,ah2:0,ah3:0,ah4:0,ah5:0,ah6:0,ah7:0,ah8:0,ah9:0,ah10:0,ah11:0,ah12:0,ah13:0,ah14:0,
     }
   },
 methods:{
 loadImg(){
+  let that = this
     var alpha = 20;
     var img1 = this.$refs.img1
     var img2 = this.$refs.img2
@@ -67,7 +71,7 @@ loadImg(){
   },4000)
   setTimeout(function(){
     startMove(100,p3)
-  },10000)
+  },17000)
 },
 drawIt(){
   var getPixelRatio = function (context) {
@@ -97,6 +101,37 @@ drawIt(){
     // 放大倍数
     context.scale(ratio, ratio);
 
+    this.aw1 = 0.426*cwidth;
+    this.ah1 = 0.285*cheight;
+    this.aw2 = 0.516*cwidth;
+    this.ah2 = 0.285*cheight;
+    this.aw3 = 0.561*cwidth;
+    this.ah3 = 0.285*cheight;
+    this.aw4 = 0.471*cwidth;
+    this.ah4 = 0.285*cheight;
+    this.aw5 = 0.345*cwidth;
+    this.ah5 = 0.405*cheight;
+    this.aw6 = 0.389*cwidth;
+    this.ah6 = 0.405*cheight;
+    this.aw7 = 0.434*cwidth;
+    this.ah7 = 0.405*cheight;
+    this.aw8 = 0.265*cwidth;
+    this.ah8 = 0.525*cheight;
+    this.aw9 = 0.310*cwidth;
+    this.ah9 = 0.525*cheight;
+    this.aw10 = 0.355*cwidth;
+    this.ah10 = 0.525*cheight;
+    this.aw11 = 0.207*cwidth;
+    this.ah11 = 0.645*cheight;
+    this.aw12 = 0.252*cwidth;
+    this.ah12 = 0.645*cheight;
+    this.aw13 = 0.152*cwidth;
+    this.ah13 = 0.765*cheight;
+    this.aw14 = 0.197*cwidth;
+    this.ah14 = 0.765*cheight;
+
+
+    let that = this
     var sizes2 = 0.0853 * cwidth
     var fontsize2 = sizes2 + 'px'
     var styles2 = "FZYTK--GBK1-0"
@@ -113,16 +148,290 @@ drawIt(){
     var font = fontsize + " " + styles
     context.font = font
     context.fillStyle = "#ffffff"
-    context.fillText("沙雕瀚程沙雕瀚程沙雕瀚程沙雕瀚程大沙雕", 0.016*cwidth, 0.285*cheight);
-    context.fillText("沙雕瀚程沙雕瀚程沙雕瀚程沙雕瀚程大沙雕", 0.072*cwidth, 0.405*cheight);
-    context.fillText("沙雕瀚程沙雕瀚程沙雕瀚程沙雕瀚程大沙雕", 0.128*cwidth, 0.525*cheight);
-    context.fillText("沙雕瀚程沙雕瀚程沙雕瀚程沙雕瀚程大沙雕", 0.072*cwidth, 0.645*cheight);
-    context.fillText("沙雕瀚程沙雕瀚程沙雕瀚程沙雕瀚程大沙雕", 0.016*cwidth, 0.765*cheight);
-}
+    context.fillText("沙雕瀚程沙雕瀚程沙", 0.016*cwidth, 0.285*cheight);
+    context.fillText("雕", that.aw1, that.ah1)
+    context.fillText("瀚", that.aw4, that.ah4)
+    context.fillText("程", that.aw2, that.ah2)
+    context.fillText("沙", that.aw3, that.ah3)
+    context.fillText("雕瀚程大沙雕",0.607*cwidth, 0.285*cheight)
+    context.fillText("沙雕瀚程沙雕", 0.073*cwidth, 0.405*cheight);
+    context.fillText("瀚", that.aw5, that.ah5)
+    context.fillText("程", that.aw6, that.ah6)
+    context.fillText("沙", that.aw7, that.ah7)
+    context.fillText("雕瀚程沙雕瀚程大沙雕",0.479*cwidth, 0.405*cheight)
+    context.fillText("沙雕瀚", 0.128*cwidth, 0.525*cheight);
+    context.fillText("程", that.aw8, that.ah8)
+    context.fillText("沙", that.aw9, that.ah9)
+    context.fillText("雕", that.aw10, that.ah10)
+    context.fillText("瀚程沙雕瀚程沙雕瀚程大沙雕",0.400*cwidth, 0.525*cheight)
+    context.fillText("沙雕瀚", 0.072*cwidth, 0.645*cheight);
+    context.fillText("程", that.aw11, that.ah11)
+    context.fillText("沙", that.aw12, that.ah12)
+    context.fillText("雕瀚程沙雕瀚程沙雕瀚程大沙雕",0.297*cwidth, 0.645*cheight)
+    context.fillText("沙雕瀚", 0.016*cwidth, 0.765*cheight);
+    context.fillText("程", that.aw13, that.ah13)
+    context.fillText("沙", that.aw14, that.ah14)
+    context.fillText("雕瀚程沙雕瀚程沙雕瀚程大沙雕",0.242*cwidth, 0.765*cheight)
+
+    function change(ctx){
+      ctx.clearRect(0.016*cwidth, 0.155*cheight,cwidth,cheight);
+      ctx.fillText("沙雕瀚程沙雕瀚程沙", 0.016*cwidth, 0.285*cheight);
+      ctx.fillText("雕", that.aw1, that.ah1)
+      ctx.fillText("瀚", that.aw4, that.ah4)
+      ctx.fillText("程", that.aw2, that.ah2)
+      ctx.fillText("沙", that.aw3, that.ah3)
+      ctx.fillText("雕瀚程大沙雕",0.607*cwidth, 0.285*cheight)
+      ctx.fillText("沙雕瀚程沙雕", 0.073*cwidth, 0.405*cheight);
+      ctx.fillText("瀚", that.aw5, that.ah5)
+      ctx.fillText("程", that.aw6, that.ah6)
+      ctx.fillText("沙", that.aw7, that.ah7)
+      ctx.fillText("雕瀚程沙雕瀚程大沙雕",0.479*cwidth, 0.405*cheight)
+      ctx.fillText("沙雕瀚", 0.128*cwidth, 0.525*cheight);
+      ctx.fillText("程", that.aw8, that.ah8)
+      ctx.fillText("沙", that.aw9, that.ah9)
+      ctx.fillText("雕", that.aw10, that.ah10)
+      ctx.fillText("瀚程沙雕瀚程沙雕瀚程大沙雕",0.400*cwidth, 0.525*cheight)
+      ctx.fillText("沙雕瀚", 0.072*cwidth, 0.645*cheight);
+      ctx.fillText("程", that.aw11, that.ah11)
+      ctx.fillText("沙", that.aw12, that.ah12)
+      ctx.fillText("雕瀚程沙雕瀚程沙雕瀚程大沙雕",0.297*cwidth, 0.645*cheight)
+      ctx.fillText("沙雕瀚", 0.016*cwidth, 0.765*cheight);
+      ctx.fillText("程", that.aw13, that.ah13)
+      ctx.fillText("沙", that.aw14, that.ah14)
+      ctx.fillText("雕瀚程沙雕瀚程沙雕瀚程大沙雕",0.242*cwidth, 0.765*cheight)
+    }
+
+    setTimeout(function(){
+      var timer = setInterval(function(){
+      if(that.aw4 <= 0.426*cwidth){
+        clearInterval(timer)
+      }
+      else{
+      that.aw4 -= 0.1;
+      that.ah4 -= 0.1;
+      change(context)
+      }
+    },20)
+    },15500)
+
+    setTimeout(function(){
+      var timer = setInterval(function(){
+      if(that.aw2 <= 0.470*cwidth){
+        clearInterval(timer)
+      }
+      else{
+      that.aw2 -= 0.1;
+      that.ah2 -= 0.1;
+      change(context)
+      }
+    },20)
+    },15000)
+
+    setTimeout(function(){
+      var timer = setInterval(function(){
+      if(that.aw3 <= 0.516*cwidth){
+        clearInterval(timer)
+      }
+      else{
+      that.aw3 -= 0.1;
+      that.ah3 -= 0.1;
+      change(context)
+      }
+    },20)
+    },15000)
+
+    setTimeout(function(){
+      var timer = setInterval(function(){
+      if(that.aw1 <= 0.380*cwidth){
+        clearInterval(timer)
+      }
+      else{
+      that.aw1 -= 0.1;
+      that.ah1 -= 0.1;
+      change(context)
+      }
+    },20)
+    },15500)
+
+    setTimeout(function(){
+      var timer = setInterval(function(){
+      if(that.aw5 <= 0.296*cwidth){
+        clearInterval(timer)
+      }
+      else{
+      that.aw5 -= 0.1;
+      that.ah5 -= 0.1;
+      change(context)
+      }
+    },20)
+    },15500)
+
+    setTimeout(function(){
+      var timer = setInterval(function(){
+      if(that.aw6 <= 0.340*cwidth){
+        clearInterval(timer)
+      }
+      else{
+      that.aw6 -= 0.1;
+      that.ah6 -= 0.1;
+      change(context)
+      }
+    },20)
+    },15500)
+
+    setTimeout(function(){
+      var timer = setInterval(function(){
+      if(that.aw7 <= 0.383*cwidth){
+        clearInterval(timer)
+      }
+      else{
+      that.aw7 -= 0.1;
+      that.ah7 -= 0.1;
+      change(context)
+      }
+    },20)
+    },15500)
+
+    setTimeout(function(){
+      var timer = setInterval(function(){
+      if(that.aw8 <= 0.220*cwidth){
+        clearInterval(timer)
+      }
+      else{
+      that.aw8 -= 0.1;
+      that.ah8 -= 0.1;
+      change(context)
+      }
+    },20)
+    },15550)
+
+    setTimeout(function(){
+      var timer = setInterval(function(){
+      if(that.aw9 <= 0.265*cwidth){
+        clearInterval(timer)
+      }
+      else{
+      that.aw9 -= 0.1;
+      that.ah9 -= 0.1;
+      change(context)
+      }
+    },20)
+    },15550)
+
+    setTimeout(function(){
+      var timer = setInterval(function(){
+      if(that.aw10 <= 0.310*cwidth){
+        clearInterval(timer)
+      }
+      else{
+      that.aw10 -= 0.1;
+      that.ah10 -= 0.1;
+      change(context)
+      }
+    },20)
+    },15550)
+
+    setTimeout(function(){
+      var timer = setInterval(function(){
+      if(that.aw11 <= 0.161*cwidth){
+        clearInterval(timer)
+      }
+      else{
+      that.aw11 -= 0.1;
+      that.ah11 -= 0.1;
+      change(context)
+      }
+    },20)
+    },15550)
+
+    setTimeout(function(){
+      var timer = setInterval(function(){
+      if(that.aw12 <= 0.206*cwidth){
+        clearInterval(timer)
+      }
+      else{
+      that.aw12 -= 0.1;
+      that.ah12 -= 0.1;
+      change(context)
+      }
+    },20)
+    },15550)
+
+    setTimeout(function(){
+      var timer = setInterval(function(){
+      if(that.aw13 <= 0.109*cwidth){
+        clearInterval(timer)
+      }
+      else{
+      that.aw13 -= 0.1;
+      that.ah13 -= 0.1;
+      change(context)
+      }
+    },20)
+    },15550)
+
+    setTimeout(function(){
+      var timer = setInterval(function(){
+      if(that.aw14 <= 0.154*cwidth){
+        clearInterval(timer)
+      }
+      else{
+      that.aw14 -= 0.1;
+      that.ah14 -= 0.1;
+      change(context)
+      }
+    },20)
+    },15550)
+},
+  show(){
+    let that = this
+    var myCanvas = document.getElementById("canv");
+    var context = myCanvas.getContext("2d");
+
+    myCanvas.width = document.documentElement.clientWidth;
+    myCanvas.height = document.documentElement.clientHeight/2;
+    var cwidth = myCanvas.width;
+    var cheight = myCanvas.height
+      var ball = {
+      x:0.8*cwidth,
+      y:0.1*cheight,
+      g:0.006*cheight,
+      vx:0.043*cwidth,
+      vy:0,
+    }
+      var timer = setInterval(function(){
+        update();
+        render(context)
+      },100)
+      function update(){
+        ball.x -= ball.vx;
+        ball.y += ball.vy;
+        ball.vy += ball.g; 
+      }
+
+      function render(ctx){
+      ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
+      var img = document.getElementById("ball");
+      var imag = new Image()
+      imag.src = img.src
+      imag.onload = function(){
+        if(ball.x < 0.152*cwidth){
+        context.drawImage(imag,ball.x,ball.y,20,20)
+        clearInterval(timer)
+        }else{
+        context.drawImage(imag,ball.x,ball.y,20,20)
+        }
+      }
+      }
+    },
   },
 mounted(){
+    let that = this
     this.loadImg()
     this.drawIt()
+    setTimeout(function(){
+      that.show()
+    },14500)
   }
 }
 </script>
@@ -198,5 +507,14 @@ mounted(){
 }
 .cans{
   opacity: 0;
+}
+#can{
+  z-index: 10
+}
+#canv{
+  position: absolute;
+  top: 296px;
+  left: 0;
+  z-index: 99
 }
 </style>
