c3689ab65918d3ab8168678dc868625f06400364
Kuo Chikuan <632385273@qq.com>
2019-03-02 02:24:17 +0800

    差点陷入泥潭出不来

--- NW2019Recruit/src/components/full.vue
+++ NW2019Recruit/src/components/full.vue
@@ -8,25 +8,19 @@
         <p class="top-text" id="full">无所不能的<span style="color:yellow">全栈</span>吧</p>
       </div>
       <div class="title">
-        <p class="three">全栈<span style="color:#000000;opacity:0.7">组</span></p>
+        <p class="three">全棧<span style="color:#000000;opacity:0.7">組</span></p>
       </div>
       </div>
     </transition>
     <div class="more-content">
-      <p id="p1"></p>
-      <div style="display:none" ref="p11">全栈工程师，是“全方位”的工程师。</div>
-      <p id="p2"></p>
-      <div style="display:none" ref="p22">既要了解前端开发，也要清楚后台逻辑，</div>
-      <p id="p3"></p>
-      <div style="display:none" ref="p33">熟悉客户端用户体验的同时又懂服务端。</div>
-      <p id="p4"></p>
-      <div style="display:none" ref="p44">在这里，我们学习CSS3,HTML5和JavaScript等</div>
-      <p id="p5"></p>
-      <div style="display:none" ref="p55">前端基础知识，使用Vue，React等主流框架进行前端开发，</div>
-      <p id="p6"></p>
-      <div style="display:none" ref="p66">掌握轻量级的Node.js作为后台语言，更方便地</div>
-      <p id="p7"></p>
-      <div style="display:none" ref="p77">搭建响应速度快、易于扩展的网络应用。</div>
+      <p id="p1">{{msg1}}</p>
+      <p id="p2">{{msg2}}</p>
+      <p id="p3">{{msg3}}</p>
+      <p id="p4">{{msg4}}</p>
+      <p id="p5">{{msg5}}</p>
+      <p id="p6">{{msg6}}</p>
+      <p id="p7">{{msg7}}</p>
+
     </div>
   </div>
 </template>
@@ -35,79 +29,103 @@
 export default {
   data(){
     return{
-      circles:false,
+      circles:false,msg1:"",msg2:"",msg3:"",msg4:"",msg5:"",msg6:"",msg7:"",
+      timer1:null,timer2:null,timer3:null,timer4:null,timer5:null,timer6:null,timer7:null,
     }
   },
   mounted(){
+    this.text = true;
     this.circles = true;
     let that = this;
     setTimeout(function(){
       that.texts()
     },2350)
-    // this.texts()
   },
   methods:{
     texts(){
-      var index1 = 0;
-      var word1 = this.$refs.p11.innerHTML;
-      function type1(){
-        document.getElementById("p1").innerText = word1.substring(0,index1++)
-      }
-      setInterval(type1,30);
-      var index2 = 0;
-      var word2 = this.$refs.p22.innerHTML;
-      function type2(){
-        document.getElementById("p2").innerText = word2.substring(0,index2++)
-      }
-      // setInterval(type2,200);
-      setTimeout(function(){
-        setInterval(type2,30);
-      },500)
-      var index3 = 0;
-      var word3 = this.$refs.p33.innerHTML;
-      function type3(){
-        document.getElementById("p3").innerText = word3.substring(0,index3++)
-      }
-      // setInterval(type3,200);
-      setTimeout(function(){
-        setInterval(type3,30);
-      },1000)
-      var index4 = 0;
-      var word4 = this.$refs.p44.innerHTML;
-      function type4(){
-        document.getElementById("p4").innerText = word4.substring(0,index4++)
-      }
-      // setInterval(type4,200);
-      setTimeout(function(){
-        setInterval(type4,30);
-      },1600)
-      var index5 = 0;
-      var word5 = this.$refs.p55.innerHTML;
-      function type5(){
-        document.getElementById("p5").innerText = word5.substring(0,index5++)
-      }
-      // setInterval(type5,200);
-      setTimeout(function(){
-        setInterval(type5,30);
-      },2600)
-      var index6 = 0;
-      var word6 = this.$refs.p66.innerHTML;
-      function type6(){
-        document.getElementById("p6").innerText = word6.substring(0,index6++)
-      }
-      // setInterval(type6,200);
-      setTimeout(function(){
-        setInterval(type6,30);
-      },3500)
-      var index7 = 0;
-      var word7 = this.$refs.p77.innerHTML;
-      function type7(){
-        document.getElementById("p7").innerText = word7.substring(0,index7++)
-      }
-      // setInterval(type7,200);
-      setTimeout(function(){
-        setInterval(type7,30);
-      },4200)
+      let that = this;
+      var text1 = "全栈工程师，是“全方位”的工程师。";
+      var i1 = 0;
+      var num1 = text1.length;
+      var text2 = "既要了解前端开发，也要清楚后台逻辑，";
+      var num2 = text2.length;
+      var i2 = 0;
+      var text3 = "熟悉客户端用户体验的同时又懂服务端。";
+      var num3 = text3.length;
+      var i3 = 0;
+      var text4 = "在这里，我们学习CSS3,HTML5和JavaScript等";
+      var num4 = text4.length;
+      var i4 = 0;
+      var text5 = "前端基础知识，使用Vue，React等主流框架进行前端开发，";
+      var num5 = text5.length;
+      var i5 = 0;
+      var text6 = "掌握轻量级的Node.js作为后台语言，更方便地";
+      var num6 = text6.length;
+      var i6 = 0;
+      var text7 = "搭建响应速度快、易于扩展的网络应用。";
+      var num7 = text7.length;
+      var i7 = 0;
+      that.timer1 = setInterval(() => {
+          var shower1 = text1.substr(0,i1++);
+          if(i1 > num1){
+            clearInterval(that.timer1);
+          }
+        that.msg1 = shower1;
+      },30)
+      setTimeout(() => {
+        that.timer2 = setInterval(() => {
+            var shower2 = text2.substr(0,i2++);
+            if(i2 > num2){
+              clearInterval(that.timer2);
+            }
+          that.msg2 = shower2;
+      },30)
+      }, 500);
+      setTimeout(() => {
+        that.timer3 = setInterval(() => {
+            var shower3 = text3.substr(0,i3++);
+            if(i3 > num3){
+              clearInterval(that.timer3);
+            }
+          that.msg3 = shower3;
+        },30)
+      }, 1000);
+      setTimeout(() => {
+        that.timer4 = setInterval(() => {
+            var shower4 = text4.substr(0,i4++);
+            if(i4 > num4){
+              clearInterval(that.timer4);
+            }
+          that.msg4 = shower4;
+        },30)
+      }, 1600);
+      setTimeout(() => {
+        that.timer5 = setInterval(() => {
+            var shower5 = text5.substr(0,i5++);
+            if(i5 > num5){
+              clearInterval(that.timer5);
+            }
+          that.msg5 = shower5;
+        },30)
+      }, 2600);
+      setTimeout(() => {
+        that.timer6 = setInterval(() => {
+            var shower6 = text6.substr(0,i6++);
+            if(i6 > num6){
+              clearInterval(that.timer6);
+            }
+          that.msg6 = shower6;
+        },30)
+      }, 3500);
+      setTimeout(() => {
+        that.timer7 = setInterval(() => {
+            var shower7 = text7.substr(0,i7++);
+            if(i7 > num7){
+              clearInterval(that.timer7);
+            }
+          that.msg7 = shower7;
+        },30)
+      }, 4200);
     }
   }
 }
@@ -119,14 +137,10 @@ export default {
   padding: 0;
   list-style: none;
 }
-/* @font-face {
-  font-family: adobe;
-  src: url("../font/adb.otf");
-}
 @font-face {
-  font-family: skm;
+  font-family: SoukouMincho;
   src: url("../font/SoukouMincho.ttf");
-} */
+}
 .fullstack{
   height: 100vh;
   background: url("../assets/full.png");
@@ -170,6 +184,7 @@ export default {
   position: absolute;
   top: 505px;
   left: 114px;
+  font-family: "SoukouMincho";
 }
 .three{
   font-size: 50px;
