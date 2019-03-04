eb937e819d5d9e882849218ed8d54dc23d5bb62f
eczn <1975845422@qq.com>
2019-03-04 13:56:35 +0800

    + for deploy

--- NW2019Recruit/config/index.js
+++ NW2019Recruit/config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: 'http://pnqc4vaxj.bkt.clouddn.com/',
 
     /**
      * Source Maps
